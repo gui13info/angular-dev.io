02e2551caed121dfa6d5078e5106857b2bd7cdef
Guilherme Marques <guilherme.marques@voxtecnologia.com.br>
2021-08-01 21:58:18 -0300

    Criando um path de rotas

---
+++ src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from "@angular/router";
+
+import { HomeComponent } from "./components/home/home.component";
+import { ContatoComponent } from "./institucional/contato/contato.component";
+import { SobreComponent } from "./institucional/sobre/sobre.component";
+
+export const rootRouterConfig: Routes = [
+    {
+        path: '', 
+        redirectTo: '/home', 
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'sobre',
+        component: SobreComponent
+    },
+    {
+        path: 'contato',
+        component: ContatoComponent
+    }
+];
